3d170b569f0bd38fc90f9a9d34b6e4d174ab2c12
Shu <stbook.syu@icloud.com>
2020-09-14 16:04:25 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/pages/HomePage/Home";
-
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from "./components/pages/SignUp/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/pages/Footer.js/Footer";
@@ -12,6 +14,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
       <Footer />
     </Router>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Button.css";
+
 const STYLES = ["btn--primary", "btn--outline"];
+
 const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+
 const COLOR = ["primary", "blue", "red", "green"];
 
 export const Button = ({
@@ -15,10 +18,11 @@ export const Button = ({
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
-  const checkButtonSize = STYLES.includes(buttonStyle) ? buttonSize : SIZES[0];
-  const checkButtonColor = STYLES.includes(buttonColor)
-    ? buttonColor
-    : COLOR[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
+
   return (
     <button
       className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
